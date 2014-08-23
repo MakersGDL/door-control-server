0807098bdc920c002ae9e631ea886437240c9614
Aaron Munguia <aamm89@gmail.com>
2014-08-23 15:38:57 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Configurar a valores por default.
+Configurar la tarjeta.
+- Conexión a la red Wifi.
+- Conexión a la red Ethernet
+Cargar Sketch (micro-usb, red[wifi])
+OpenWrt y web service (root:<password>)
+Visualizar sitio web
---
+++ ServerDoor.ino
@@ -0,0 +1,74 @@
+/*
+  Door web interface
+  
+  http://makerspace.makersgdl.com/
+ */
+ 
+#include <Bridge.h>
+#include <YunServer.h>
+#include <YunClient.h>
+#include <Servo.h>
+
+Servo doorServo;  // create servo object to control a servo 
+int pos = 0;    // variable to store the servo position
+
+// Listen on default port 5555, the webserver on the Yún
+// will forward there all the HTTP requests for us.
+YunServer server;
+
+void setup() {
+  Serial.begin(9600);
+  
+  doorServo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  
+  // Bridge startup
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Bridge.begin();
+  digitalWrite(13, HIGH);
+  
+  // Listen for incoming connection only from localhost
+  // (no one from the external network could connect)
+  server.listenOnLocalhost();
+  server.begin();
+}
+
+void loop() {
+  // Get clients coming from server
+  YunClient client = server.accept();
+  
+  // There is a new client ? 
+  if(client){
+    
+    // read the command 
+    String command = client.readString();
+    command.trim(); // kill whitespace
+    Serial.println(command);
+    
+    // is "open" command?
+    if (command == "open"){
+      client.print("Please wait, opening door...");
+      openDoor();
+      client.println("Open!");
+      delay(1000); // Wait 1000ms (1s)
+    }
+    
+    // Close connection and free resources.
+    client.stop();
+  }
+  
+  delay(50); // Poll every 50ms
+}
+
+void openDoor(){
+  for(pos = 0; pos <= 180; pos += 1) // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    doorServo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+  for(pos = 180; pos>=0; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    doorServo.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+  } 
+}
